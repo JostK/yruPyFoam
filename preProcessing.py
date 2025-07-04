163eea84e8f6c9802cbc4a03eed92b8d31bb8ae7
JostK <jost-kemper@gmx.de>
2025-07-04 20:58:54 +0200

    extending gradingCalculator

--- preProcessing.py
+++ preProcessing.py
@@ -100,6 +100,30 @@ def findCellNumberFirst(distG, firstCell, growRate):
     resLCell = resFCell*growRate**(N-1)
     return [N, resFCell, resLCell]
 
+def findCellNumberLast(distG, lastCell, growRate):
+    """
+    function to get Number of cells based on grow rate, maximum size of wall cell and general distance
+    input:
+        distG:        general distance
+        lastCell:     size of last cell
+        growRate:     cell-to-cell grow rate    
+    output:
+        N:            Number of cells
+        resFCell:     resulting size of first cell
+        resLCell:     resulting size of last cell
+    """
+    #if growRate<1.0: raise ValueError("please provide grow rate >1")
+    #guess=(distG/firstCell)/growRate**(distG/firstCell)
+    # firstCell = lastCell/growRate**(N-1)
+    func = lambda N : (1 -growRate**N) / (1 - growRate) - growRate**(N-1) * distG / lastCell    
+    [N] = fsolve(func, 100)
+    #as a full number of cells is needed and firstCell represents a maximum distance, the number is rounded up
+    N = np.ceil(N)
+    # resulting size of first cell
+    resFCell = distG / ((1-growRate**N)/(1-growRate))
+    resLCell = resFCell*growRate**(N-1)
+    return [N, resFCell, resLCell]
+
 def findCellNumberFirstLast(distG, firstCell, lastCell):
     """
     function to get Number of cells based on size of first and last cell, and general distance
@@ -124,7 +148,7 @@ def findCellNumberFirstLast(distG, firstCell, lastCell):
     resLCell = resFCell*growRate**(N-1)
     return [N, resFCell, resLCell, growRate]
 
-def gradingCalculator(firstCell=None, lastCell=None, nCells=None, distance=None, growRate=None):
+def gradingCalculator(firstCell=None, lastCell=None, nCells=None, distance=None, growRate=None, grading=None):
     """
     function to calculate grading information from variable inputs. Not all inputs have to be given and the idea is that the function will calculate the other other (not provided) parameters. Not all functions are implemented yet.
     input:
@@ -142,28 +166,63 @@ def gradingCalculator(firstCell=None, lastCell=None, nCells=None, distance=None,
     _____
     distG = sum_{n=0}^{N-1} firstCell*growRate^n => distG/firstCell =  sum_{n=0}^{N-1} growRate^n = (1 - growRate^N) / (1-growRate)
     """
+    # Pre-calculations if grading is given instead of growRate:
+    if grading is not None:
+        if (nCells is not None) and (growRate is not None):
+            raise KeyError(f"Information provided through the 'grading' parameter is redundant")
+        elif nCells is not None:
+            growRate = grading**(1.0/(nCells-1.0))
+        elif growRate is not None:
+            nCells = np.log(grading) / np.log(growRate) + 1
+            
+        if (lastCell is not None) and (firstCell is not None):
+            raise KeyError(f"Information provided through the 'grading' parameter is redundant")
+        elif lastCell is not None:
+                firstCell = lastCell/grading
+        elif firstCell is not None:
+                lastCell = firstCell * grading
     
     #print(f"Grading calculation with firstCell = {firstCell}, lastCell = {lastCell}, nCells = {nCells}, distance = {distance}, growRate = {growRate}")
     
-    if (distance is not None) and (firstCell is not None) and (nCells is not None):
-        guess= (distance / nCells) / firstCell # if the first cell is larger than the mean cell, the grading is likely < 1 and vice versa.
-        precision=4
-        growRate, firstCell, lastCell = findGrowRateFirst(distance, firstCell, nCells, guess, precision)
-    elif (distance is not None) and (lastCell is not None) and (nCells is not None):
-        guess= lastCell / (distance / nCells) # if the first cell is larger than the mean cell, the grading is likely < 1 and vice versa.
-        precision=4
-        growRate, firstCell, lastCell = findGrowRateLast(distance, lastCell, nCells, guess, precision)
-    elif (distance is not None) and (firstCell is not None) and (growRate is not None):
-        nCells, firstCell, lastCell = findCellNumberFirst(distance, firstCell, growRate)
-    elif (firstCell is not None) and (lastCell is not None) and (nCells is not None):
-        growRate = (lastCell/firstCell)**(1.0/(nCells-1.0))
-        distance = firstCell * ((1-growRate**nCells)/(1-growRate))
-    elif (firstCell is not None) and (lastCell is not None) and (growRate is not None):
-        nCells = np.log(lastCell/firstCell) / np.log(growRate) + 1
-        distance = firstCell * ((1-growRate**nCells)/(1-growRate))       
-    elif (firstCell is not None) and (lastCell is not None) and (distance is not None):
-        nCells, firstCell, lastCell, growRate = findCellNumberFirstLast(distance, firstCell, lastCell)
-    else:
+    found = False
+    
+    if (firstCell is not None) and (lastCell is not None):
+        if nCells is not None:
+            growRate = (lastCell/firstCell)**(1.0/(nCells-1.0))
+            distance = firstCell * ((1-growRate**nCells)/(1-growRate))
+            found = True
+        elif growRate is not None:
+            nCells = np.log(lastCell/firstCell) / np.log(growRate) + 1
+            distance = firstCell * ((1-growRate**nCells)/(1-growRate))  
+            found = True
+        elif distance is not None:
+            nCells, firstCell, lastCell, growRate = findCellNumberFirstLast(distance, firstCell, lastCell)
+            found = True
+    elif (distance is not None) and (growRate is not None):
+        if firstCell is not None:
+            nCells, firstCell, lastCell = findCellNumberFirst(distance, firstCell, growRate)
+            found = True
+        elif lastCell is not None:
+            nCells, firstCell, lastCell = findCellNumberLast(distance, lastCell, growRate)
+            found = True
+        elif nCells is not None:
+            firstCell =  distance / ((1-growRate**nCells)/(1-growRate))
+            nCells, firstCell, lastCell = findCellNumberFirst(distance, firstCell, growRate)
+            found = True
+    elif (distance is not None) and (nCells is not None):
+        if firstCell is not None:
+            guess= (distance / nCells) / firstCell # if the first cell is larger than the mean cell, the grading is likely < 1 and vice versa.
+            precision=4
+            growRate, firstCell, lastCell = findGrowRateFirst(distance, firstCell, nCells, guess, precision)
+            found = True
+        elif lastCell is not None:
+            guess= lastCell / (distance / nCells) # if the first cell is larger than the mean cell, the grading is likely < 1 and vice versa.
+            precision=4
+            growRate, firstCell, lastCell = findGrowRateLast(distance, lastCell, nCells, guess, precision)
+            found = True
+            
+    if not found:
         raise KeyError(f"Grading calculation with firstCell = {firstCell}, lastCell = {lastCell}, nCells = {nCells}, distance = {distance}, growRate = {growRate} is not (yet) implemented")
+        
     return [firstCell, lastCell, nCells, distance, growRate]
         
